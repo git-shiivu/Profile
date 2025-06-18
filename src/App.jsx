6052570b5c5ddfbaafd3e23aecbe7c732cb715c1
Shivam Kumar <shivamku9107@gmail.com>
2025-06-19 00:15:08 +0530

    Project Uploaded

---
+++ src/App.jsx
@@ -0,0 +1,90 @@
+import './App.css'
+
+function App() {
+
+  const projectLinks = [{title: 'Famous food of 5 countries', link: 'https://github.com/git-shiivu/mealMenu.git', working:'It is a website that use data from API for showing results.'},]
+  
+  return (
+    <div className=" text-gray-800">
+      {/* Navbar */}
+      <header className="bg-white shadow-md sticky top-0 z-50">
+        <div className="max-w-7xl mx-auto flex justify-between items-center px-4 py-3">
+          <h1 className="text-2xl font-bold max-w-[50vw]">Shivam Kumar</h1>
+          <nav className="flex justify-between max-w-[50vw]">
+            <a href="#about" className="hover:text-blue-600 mx-[1vw]">About</a>
+            <a href="#projects" className="hover:text-blue-600 mx-[1vw]">Projects</a>
+            <a href="#contact" className="hover:text-blue-600 mx-[1vw]">Contact</a>
+          </nav>
+        </div>
+      </header>
+
+      
+
+      {/* Profile Section */}
+      <section className="bg-gradient-to-r from-blue-100 to-white py-[5vh] px-4">
+        <div className="flex justify-around flex-wrap">
+          <div className=''>
+            <h2 className="text-4xl font-bold mb-4 max-sm:max-w-[100vw] sm:max-w-[50vw]">Hi, I'm Shivam Kumar</h2>
+            <p className="text-lg text-gray-700 mb-6 ">I’m a Front-end Web developer passionate about building responsive and modern web apps.</p>
+            <a href="#projects" className="bg-blue-600 text-white px-5 py-3 rounded-xl hover:bg-blue-700 duration-200">See My Work</a>
+          </div>
+          <img src='.\src\Images\ShivamKumar.jpg' alt="Shivam Kumar" className="max-sm:mt-[9vw] rounded-xl shadow-md max-sm:max-w-[55vw] sm:max-w-[25vw] inline-flex self-end hover:scale-105 duration-150 " />
+        </div>
+      </section>
+
+      {/* About Section */}
+      <section id="about" className="py-16 px-4 bg-white">
+        <div className="max-w-4xl mx-auto text-center">
+          <h3 className="text-3xl font-semibold mb-4">About Me</h3>
+          <p className="text-gray-600 text-lg">
+            I’m a developer with experience in React, Tailwind CSS, Javascript I enjoy turning ideas into real products and solving real-world problems through code.
+          </p>
+        </div>
+      </section>
+
+      {/* Projects Section */}
+      <section id="projects" className="py-16 px-4 bg-gray-100">
+        <div className="max-w-6xl mx-auto">
+          <h3 className="text-3xl font-semibold text-center mb-10">Projects</h3>
+          <div className="inline-flex justify-center flex-wrap ">
+            {projectLinks.map(i => (
+              <div key={i} className="bg-white rounded-xl shadow-md p-5 max-sm:max-w-[100vw] max-sm:min-h-[40vh] sm:max-w-[30vw] sm:min-h-[50vh] m-[2vw] hover:scale-105 duration-150">
+                <h4 className="text-xl font-bold ">Project Title:</h4>
+                <h5 className="text-xl font-semibold mb-[2vh]">{i.title}</h5>
+                <hr />
+                <p className="text-gray-600 my-[2vh]">{i.working}</p>
+                <p className="text-gray-600 text-sm">
+                  <b>Git Hub: </b>
+                  <a href={i.link} target='_blank'>{i.link}</a></p>
+              </div>
+            ))}
+          </div>
+
+        </div>
+      </section>
+
+      {/* Contact Section */}
+      <section id="contact" className="py-16 px-4 bg-white">
+        <div className="text-center">
+          <h3 className="text-3xl font-semibold mb-4">Get in Touch</h3>
+          <p className="text-gray-600 mb-6">Interested in working together or just want to say hi?</p>
+          <div>
+            <a href="https://www.instagram.com/shiivu.dev?igsh=MTVlbW83cmdjcXowcA==" target='_blank' className=" bg-blue-600 text-white px-6 py-3 rounded-xl hover:bg-blue-700 duration-150">Visit on Instagram</a>
+          </div>
+          <p className="text-gray-600 my-[2vw]">Or</p>
+          <div className=''>
+          <a href="https://mail.google.com/mail/?view=cm&fs=1&to=shiivuishere@gmail.com&su=Hello%20Shivam,&body=I%20have%20a%20work%20for%20you," target='_blank' className=" bg-blue-600 text-white px-6 py-3 rounded-xl hover:bg-blue-700 duration-150">Email Me</a>
+          </div>
+        </div>
+      </section>
+
+      {/* Footer */}
+      <footer className="bg-gray-100 text-center py-6">
+        <p className="text-sm text-gray-500">© {new Date().getFullYear()} Shivam Kumar. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+}
+
+
+export default App
